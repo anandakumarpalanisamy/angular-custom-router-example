c52deeff996285c5f45a6399e9e5c0c24c43fb2f
Anandakumar Palanisamy <ananda11k@gmail.com>
2022-01-20 18:22:11 +0000

    Angular custom route matching example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { RouterModule, UrlSegment } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {
+      matcher: (url) => {
+        if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+          return {
+            consumed: url,
+            posParams: {
+              username: new UrlSegment(url[0].path.substring(1), {})
+            }
+          };
+        }
+        return null;
+      },
+      component: ProfileComponent
+    }
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
